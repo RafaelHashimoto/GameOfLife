b0fc50abaed2e9de22b833b1e3e3d295ddc03a99
RafaelHashimoto <hashimoto_wmd@hotmail.com>
2016-11-14 06:08:39 -0300

    Terminado

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cell : MonoBehaviour {
+
+	public Sprite[] status;
+
+	public static int cellType;
+
+	private int minNeighborsA;
+	private int maxNeighborsA;
+
+	private int minNeighborsB;
+	private int maxNeighborsB;
+
+	public bool alive;	
+	public int currentCellType; 
+	// Use this for initialization
+	void Start () {
+		this.GetComponent<SpriteRenderer> ();
+
+		alive = false;
+		cellType = 0;
+		currentCellType = cellType;
+
+		minNeighborsA = 2;
+		maxNeighborsA = 3;
+		minNeighborsB = 2;
+		maxNeighborsB = 6;
+	}
+
+	void OnMouseDown(){
+		if (Input.GetMouseButtonDown(0)) {
+			CellInstantiate ();
+		} 	
+	}
+
+	void CellInstantiate(){
+		this.GetComponent<SpriteRenderer> ().sprite = status[cellType];
+		currentCellType = cellType;
+		if (cellType == 0) {
+			alive = false;
+		} else {
+			alive = true;
+		}
+	}
+
+	public void CellUpdate(){
+		this.GetComponent<SpriteRenderer> ().sprite = status[currentCellType];
+		if (currentCellType == 0) {
+			alive = false;
+		} else {
+			alive = true;
+		}
+	}
+
+	public bool IsAlive(){
+		return alive;
+	}
+
+	public int CellType(){
+		return currentCellType;
+	}
+
+	public void CellJudgment(int neighbour){
+		if (alive) {
+			//Se for célula do tipo A
+			Debug.Log("Celula do tipo: "+ currentCellType);
+			Debug.Log ("Vizinhos: " + neighbour);
+			if (currentCellType == 1) {
+				if (neighbour < minNeighborsA || neighbour > maxNeighborsA) {
+					Debug.Log ("mata verde");
+					currentCellType = 0;
+					CellUpdate ();
+				}
+			}
+			//Se for célula do tipo B
+			if (currentCellType == 2) {
+				// Se não tiver 2 nem 3 vizinhos vivos então checa se há condições de vida 
+				if (neighbour < minNeighborsB || neighbour > maxNeighborsB) {
+					currentCellType = 0;
+					Debug.Log ("mata azul");
+					CellUpdate ();
+				} 
+			}
+		} else {
+			// Se tiver 3 vizinhos, se torna uma célula A
+			if (neighbour == 3) {
+				currentCellType = 1;
+				CellUpdate ();
+			} 
+			// Se tiver 4 vizinhos, se torna uma célula B
+			if (neighbour == 4) {
+				currentCellType = 2;
+				CellUpdate ();
+			}
+		}
+
+	}
+}
---
+++ Assets/Scripts/PetriPlateScript.cs
@@ -0,0 +1,151 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PetriPlateScript : MonoBehaviour {
+
+	// as dimensões do array que representará o grid da placa de petri em x e y
+	private int arrayDimensionX;
+	private int arrayDimensionY;
+	// Array de celulas que reprensentará a placa de petri
+	public Cell[,] cells;
+	// prefab de celula que será usado para instanciar as celulas na placa de petri
+	public Cell cellPrefab;
+	// posição tridimensional da placa de petri. O método Instantiate só aceita vetores de 3 dimensões
+	private Vector3 cellPosition;
+
+	private int neighbour;
+
+	private int scanPlateLoop;
+
+	private bool enableScanPlate;
+	// Inicializando as variáveis e objetos
+
+	public Text optionSelected;
+	void Start () {
+
+		// Placa de 20x20
+		arrayDimensionX = 20;
+		arrayDimensionY = 20;
+		// Intanciando o array
+		cells = new Cell[arrayDimensionY,arrayDimensionX];
+		// Populando o array
+		CreatePlate ();
+		// Setando o Vector 3
+		cellPosition.x = 0f;
+		cellPosition.y = 0f;	
+		cellPosition.z = 0f;
+
+		neighbour = 0;
+		scanPlateLoop = 1;
+		enableScanPlate = false;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (enableScanPlate) {
+			if (scanPlateLoop > 0) {
+				ScanPlate ();
+				scanPlateLoop -= 1;
+			} else {
+				scanPlateLoop = 1;
+				enableScanPlate = false;
+			}
+		}
+		
+	}
+		
+	/*IEnumerator TalkAsync(int y, int x){
+		CheckNeighborhood (y, x);
+		yield return new WaitForSeconds(100.0f);
+	}*/
+
+	void CreatePlate(){
+
+		for (int y = 0; y < arrayDimensionY; y++){
+			for (int x = 0; x < arrayDimensionX; x++) {
+				cells[y,x] = Instantiate(cellPrefab, cellPosition, Quaternion.identity) as Cell;
+				cellPosition.x += 1f;
+			}
+			cellPosition.x = 0;
+			cellPosition.y += 1f;
+		}
+
+	}
+
+	// Percorre o array de células checando seus vizinhos
+	void ScanPlate(){
+		for (int y = 0; y < arrayDimensionY; y++){
+			for (int x = 0; x < arrayDimensionX; x++) {
+				CheckNeighborhood (y, x);
+				//StartCoroutine(TalkAsync (y, x));
+
+			}
+		}
+	}
+
+	// percorre os vizinhos da celula
+	void CheckNeighborhood(int y, int x){
+		// verifica os vizinhos começando de x - 1 e y -1 até x + 1 e y + 1
+		// y => i
+		// x => j
+		//Time.timeScale = 0.1f;
+		//Debug.Log("Célula na posição X:" + x + " Y: " + y);
+		for (int i = y - 1; i <= y + 1; i++){
+			for (int j = x - 1; j <= x + 1; j++) {
+				// if para verificar se os index dos vizinhos estão dentro do array no eixo Y
+				if (i >= 0 && i <= arrayDimensionY -1) {
+					// if para verificar se os index dos vizinhos estão dentro do array no eixo X
+					if (j >= 0 && j <= arrayDimensionX -1){
+						
+						// Se a celula vizinha estiver viva e garante que será comparado apenas com os seus vizinhos
+						if (!(i == y && j == x ) && cells[i,j].IsAlive() ) {
+							
+							//Debug.Log ("Vizinho na posição X: " + j + " Y: " + i);
+							neighbour += 1;
+						}	
+					}
+				}
+			}
+		}
+		Debug.Log ("Y:" + y + "X:" + x);
+		cells [y,x].CellJudgment (neighbour);
+		neighbour = 0;
+
+
+	}
+
+	public void DeadCell(){
+		Cell.cellType = 0;
+		optionSelected.text = "KILL";
+	}
+
+	public void CellTypeA(){
+		Cell.cellType = 1;
+		optionSelected.text = "CELL TYPE A";
+	}
+
+	public void CellTypeB(){
+		Cell.cellType = 2;
+		optionSelected.text = "CELL TYPE B";
+	}
+
+	public void StartScanPlate(){
+		enableScanPlate = true;
+		optionSelected.text = "SCAN";
+	}
+
+	public void Clear(){
+		for (int y = 0; y < arrayDimensionY; y++){
+			for (int x = 0; x < arrayDimensionX; x++) {
+				cells [y, x].currentCellType = 0;
+				cells [y, x].CellUpdate ();
+			}
+		}
+	}
+
+}
+
